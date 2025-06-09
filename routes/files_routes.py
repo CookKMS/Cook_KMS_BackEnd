60eebb2168547b9f0e0be48e1b1fc57ed35abe88
jaewoongyoo <allen08120812@gmail.com>
2025-06-09 21:06:53 +0900

    파일 업로드 라우트 구현

--- routes/files_routes.py
+++ routes/files_routes.py
@@ -10,28 +10,28 @@ from utils.decorators import jwt_required, role_required
 
 file_bp = Blueprint("file_bp", __name__)
 
-# [파일 업로드] - 인증 필요, 역할은 board_code에 따라 내부에서 판별
+# 파일 업로드 - 인증 필요, 역할은 board_code에 따라 내부에서 판별
 @file_bp.route("/upload", methods=["POST"])
 @jwt_required
 @role_required("user", "employee", "admin")
 def upload():
     return save_file(request)
 
-# [파일 삭제] - 인증된 사용자라면 삭제 가능
+# 파일 삭제 - 인증된 사용자라면 삭제 가능
 @file_bp.route("/<int:file_id>", methods=["DELETE"])
 @jwt_required
 @role_required("user", "employee", "admin")
 def delete(file_id):
     return delete_file_by_id(file_id)
 
-# [파일 교체] - 기존 파일을 새로운 파일로 교체
+# 파일 교체 - 기존 파일을 새로운 파일로 교체
 @file_bp.route("/<int:file_id>/replace", methods=["PUT"])
 @jwt_required
 @role_required("user", "employee", "admin")
 def replace(file_id):
     return replace_file_by_id(file_id, request)
 
-# [파일 다운로드] - 인증된 사용자 누구나 가능
+# 파일 다운로드- 인증된 사용자 누구나 가능
 @file_bp.route("/download/<int:file_id>", methods=["GET"])
 @jwt_required
 @role_required("user", "employee", "admin")
