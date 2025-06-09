b40db1e903f908c804e1d2f3ce80ecb66ff397a2
jaewoongyoo <allen08120812@gmail.com>
2025-06-09 15:24:35 +0900

    유저 관련 라우트 추가

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,1 +1,25 @@
+# 사용자 정보 조회 라우트 정의
+from flask import Blueprint, jsonify
+from utils.decorators import jwt_required, role_required
+from models.user import User
 
+user_bp = Blueprint("user_bp", __name__)
+
+# 사용자 전체 목록 조회 (관리자만 가능)
+@user_bp.route("/", methods=["GET"])
+@jwt_required
+@role_required("admin")
+def list_users():
+    users = User.query.order_by(User.id.asc()).all()
+    result = [
+        {
+            "id": user.id,
+            "username": user.username,
+            "role": user.role.value,
+            "employee_number": user.employee_number,
+            "created_at": user.created_at,
+            "updated_at": user.updated_at
+        }
+        for user in users
+    ]
+    return jsonify(result), 200
