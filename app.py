062fd49a4bf3fd182985ba1ed5696c940016b753
jaewoongyoo <allen08120812@gmail.com>
2025-06-09 14:53:13 +0900

    app.py에 블루프린트 등록

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from config import Config
 from db_init import init_db
+from werkzeug.utils import secure_filename
 
 # 블루프린트 import
 from routes.auth_routes import auth_bp
 from routes.files_routes import file_bp
 from routes.faq_routes import faq_bp
+from routes.knowledge_routes import knowledge_bp
+from routes.common_routes import common_bp
+
 
 def create_app():
     app = Flask(__name__)
@@ -16,15 +20,15 @@ def create_app():
 
     # 블루프린트 등록
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
-    app.register_blueprint(file_bp, url_prefix="/api/file:")
+    app.register_blueprint(file_bp, url_prefix="/api/file")
     app.register_blueprint(faq_bp, url_prefix="/api/faq")
+    app.register_blueprint(knowledge_bp, url_prefix="/api/knowledge")
+    app.register_blueprint(common_bp)  
 
-    # 업로드 파일 접근용 정적 라우팅
-    from flask import send_from_directory
-    import os
-
+    # 업로드된 파일 정적 접근 라우트
     @app.route("/uploads/<filename>")
     def uploaded_file(filename):
+        filename = secure_filename(filename)  
         return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
     return app
